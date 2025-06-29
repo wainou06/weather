611eef04f3c5a20969c14e4b0860fc9a30a4fecd
wainou06 <wainou.6@gmail.com>
2025-06-29 20:06:23 +0900

    up

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,12 @@
+import { NavLink } from 'react-router-dom'
+
 function NotFound() {
    return (
-      <div style={{margin: '70px auto', width: '720px', textAlign: 'center'}}>
+      <div style={{ margin: '70px auto', width: '720px', textAlign: 'center' }}>
          <h1>페이지를 찾을 수 없습니다.</h1>
-         <img src="/images/not found.png" alt="페이지 없음" />
+         <NavLink to="/">
+            <img src="/images/not found.png" alt="페이지 없음" />
+         </NavLink>
          <h1>SORRY, NOT FOUND</h1>
       </div>
    )
